d48993317bd6890376d8f73d6457b472856e6e61
Viktor Koop <viktor.koop@googlemail.com>
2018-05-05 20:22:54 +0200

    reformat with xo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const SmartDevice = require('./SmartDevice');
 const program = require('commander');
+const SmartDevice = require('./SmartDevice');
 
-program.command("set-status <status> <id> <key>")
-    .option("-i, --ip <ipAddress>", "device ip")
-    .action((status, id, key, cmd) => {
-        new SmartDevice(id, key, cmd.ip).setStatus(status);
-    });
+program.command('set-status <status> <id> <key>')
+	.option('-i, --ip <ipAddress>', 'device ip')
+	.action((status, id, key, cmd) => {
+		new SmartDevice(id, key, cmd.ip).setStatus(status);
+	});
 
-program.command("get-status <id> <key>")
-    .option("-i, --ip <ipAddress>", "device ip")
-    .action((id, key, cmd) => {
-        new SmartDevice(id, key, cmd.ip).getStatus();
-    });
+program.command('get-status <id> <key>')
+	.option('-i, --ip <ipAddress>', 'device ip')
+	.action((id, key, cmd) => {
+		new SmartDevice(id, key, cmd.ip).getStatus();
+	});
 
 program.parse(process.argv);
